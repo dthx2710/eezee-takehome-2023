f7473b186e56b39cc8f8f36bf5ed116d1395fac2
Dolicon <dthx2710@hotmail.com>
2023-04-29 00:14:24 +0800

    feat: brand alphabetical sort functionality

--- src/pages/brands/index.tsx
+++ src/pages/brands/index.tsx
@@ -4,11 +4,39 @@ import React from "react";
 import brandData from "@/data/brands.json";
 
 const Brands = () => {
+  // sort brand by name
+  const sortedBrands = brandData.sort((a, b) => a.name.localeCompare(b.name));
+  // every alphabet has its own array
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  // array of brands that start with each alphabet
+  const brands = alphabet.map((letter) =>
+    sortedBrands.filter((brand) => brand.name.toLowerCase().startsWith(letter.toLowerCase()))
+  );
+
   return (
     <Layout>
-      {brandData.map((brand) => (
-        <BrandCard key={brand.id} id={brand.id} name={brand.name} image={brand.image?.url} />
-      ))}
+      {
+        // for each alphabet, if there are brands that start with that alphabet, display the alphabet and the brands
+        brands.map((brand, index) => (
+          <div key={index}>
+            {brand.length > 0 && (
+              <>
+                <h1>{alphabet[index]}</h1>
+                <div className="grid grid-cols-6 md:grid-cols-6 lg:grid-cols-6 gap-4">
+                  {brand.map((brand) => (
+                    <BrandCard
+                      key={brand.id}
+                      id={brand.id}
+                      name={brand.name}
+                      image={brand.image?.url}
+                    />
+                  ))}
+                </div>
+              </>
+            )}
+          </div>
+        ))
+      }
     </Layout>
   );
 };
